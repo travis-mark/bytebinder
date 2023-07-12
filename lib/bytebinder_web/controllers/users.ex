f435ee850da0eb9dfadf985d8cc4bb5075bcd5d0
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2023-07-11 21:29:52 -0400

    Show user data

--- lib/bytebinder_web/controllers/users.ex
+++ lib/bytebinder_web/controllers/users.ex
@@ -1,3 +1,5 @@
+alias Bytebinder.{Repo,User}
+
 defmodule BytebinderWeb.UserController do
   use BytebinderWeb, :controller
 
@@ -16,11 +18,23 @@ defmodule BytebinderWeb.UserController do
 
   @spec show(Plug.Conn.t(), map) :: Plug.Conn.t()
   def show(conn, %{"id" => id}) do
-    render(conn, :show, id: id)
+    user = Bytebinder.Repo.get(Bytebinder.User, id)
+    render(conn, :show, user: user)
   end
 
-  def create(conn, _params) do
-    render(conn, :create)
+  @spec create(Plug.Conn.t(), any) :: Plug.Conn.t()
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "User created successfully.")
+        |> redirect(to: ~p"/users/")
+
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
   end
 
   def update(conn, %{"id" => id}) do
