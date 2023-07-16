35f1a21849d38ed54b2ff7c057f3be150da9fde2
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2023-07-16 18:10:26 -0400

    Buttons as links

--- lib/bytebinder_web/components/core_components.ex
+++ lib/bytebinder_web/components/core_components.ex
@@ -216,24 +216,42 @@ defmodule BytebinderWeb.CoreComponents do
   """
   attr :type, :string, default: nil
   attr :class, :string, default: nil
+  attr :href, :string, default: nil
   attr :rest, :global, include: ~w(disabled form name value)
 
   slot :inner_block, required: true
 
   def button(assigns) do
-    ~H"""
-    <button
-      type={@type}
-      class={[
-        "phx-submit-loading:opacity-75 rounded-lg bg-zinc-900 hover:bg-zinc-700 py-2 px-3",
-        "text-sm font-semibold leading-6 text-white active:text-white/80",
-        @class
-      ]}
-      {@rest}
-    >
-      <%= render_slot(@inner_block) %>
-    </button>
-    """
+    if assigns.href do
+      ~H"""
+      <a
+        href={@href}
+        class={[
+          "phx-submit-loading:opacity-75 rounded-lg bg-zinc-900 hover:bg-zinc-700 py-2 px-3",
+          "text-sm font-semibold leading-6 text-white active:text-white/80",
+          @class
+        ]}
+        {@rest}
+      >
+        <%= render_slot(@inner_block) %>
+      </a>
+      """
+    else
+      ~H"""
+      <button
+        type={@type}
+        class={[
+          "phx-submit-loading:opacity-75 rounded-lg bg-zinc-900 hover:bg-zinc-700 py-2 px-3",
+          "text-sm font-semibold leading-6 text-white active:text-white/80",
+          @class
+        ]}
+        {@rest}
+      >
+        <%= render_slot(@inner_block) %>
+      </button>
+      """
+    end
+
   end
 
   @doc """
