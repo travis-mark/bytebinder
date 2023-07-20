56a75abb0a023da382927b83901ddfdb13548191
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2023-07-19 21:00:53 -0400

    Start bytebinder title card

--- lib/bytebinder_web/controllers/page_html.ex
+++ lib/bytebinder_web/controllers/page_html.ex
@@ -2,4 +2,16 @@ defmodule BytebinderWeb.PageHTML do
   use BytebinderWeb, :html
 
   embed_templates "page_html/*"
+
+  attr :href, :string, default: nil
+  slot :inner_block, required: true
+  def app_button(assigns) do
+    ~H"""
+      <a href={@href}>
+        <div class="w-32 h-32 flex flex-col justify-center items-center bg-gray-400 hover:bg-gray-200 rounded-lg">
+          <%= render_slot(@inner_block) %>
+        </div>
+      </a>
+    """
+  end
 end
