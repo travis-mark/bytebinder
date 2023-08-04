37408fe94790b99158a54b2b3c1bc9cfc666e357
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2023-08-03 22:08:10 -0400

    User show cleanup

--- lib/bytebinder_web/core_components.ex
+++ lib/bytebinder_web/core_components.ex
@@ -300,7 +300,7 @@ defmodule BytebinderWeb.CoreComponents do
 
   attr :rest, :global,
     include: ~w(accept autocomplete capture cols disabled form list max maxlength min minlength
-                multiple pattern placeholder readonly required rows size step)
+                multiple pattern placeholder readonly required rows size step viewonly)
 
   slot :inner_block
 
@@ -381,10 +381,7 @@ defmodule BytebinderWeb.CoreComponents do
     <div phx-feedback-for={@name}>
       <.label for={@id}><%= @label %></.label>
       <div class={[
-          "mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6",
-          "phx-no-feedback:border-zinc-300 phx-no-feedback:focus:border-zinc-400",
-          @errors == [] && "border-zinc-300 focus:border-zinc-400",
-          @errors != [] && "border-rose-400 focus:border-rose-400"
+          "mb-2 block w-full sm:text-sm sm:leading-6",
       ]}><%= Phoenix.HTML.Form.normalize_value(@type, @value) %></div>
     </div>
     """
