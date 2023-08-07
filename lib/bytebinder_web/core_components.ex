0e231f112eb18339748b17b7eb7dc1130c5a69c7
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2023-08-06 21:47:57 -0400

    Fix margin on missing input

--- lib/bytebinder_web/core_components.ex
+++ lib/bytebinder_web/core_components.ex
@@ -344,7 +344,12 @@ defmodule BytebinderWeb.CoreComponents do
       <select
         id={@id}
         name={@name}
-        class="mt-2 block w-full rounded-md border border-gray-300 bg-white shadow-sm focus:border-zinc-400 focus:ring-0 sm:text-sm"
+        class={[
+          "block w-full rounded-md border bg-white shadow-sm focus:ring-0 sm:text-sm",
+          @errors == [] && "border-zinc-300 focus:border-zinc-400",
+          @errors != [] && "border-rose-400 focus:border-rose-400",
+          @label && "mt-2"
+        ]}
         multiple={@multiple}
         {@rest}
       >
@@ -364,10 +369,11 @@ defmodule BytebinderWeb.CoreComponents do
         id={@id}
         name={@name}
         class={[
-          "mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6",
+          "block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6",
           "min-h-[6rem] phx-no-feedback:border-zinc-300 phx-no-feedback:focus:border-zinc-400",
           @errors == [] && "border-zinc-300 focus:border-zinc-400",
-          @errors != [] && "border-rose-400 focus:border-rose-400"
+          @errors != [] && "border-rose-400 focus:border-rose-400",
+          @label && "mt-2",
         ]}
         {@rest}
       ><%= Phoenix.HTML.Form.normalize_value("textarea", @value) %></textarea>
@@ -381,7 +387,8 @@ defmodule BytebinderWeb.CoreComponents do
     <div phx-feedback-for={@name}>
       <.label for={@id}><%= @label %></.label>
       <div class={[
-          "mb-2 block w-full sm:text-sm sm:leading-6",
+          "block w-full sm:text-sm sm:leading-6",
+          @label && "mt-2",
       ]}><%= Phoenix.HTML.Form.normalize_value(@type, @value) %></div>
     </div>
     """
@@ -398,10 +405,11 @@ defmodule BytebinderWeb.CoreComponents do
         id={@id}
         value={Phoenix.HTML.Form.normalize_value(@type, @value)}
         class={[
-          "mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6",
+          "block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6",
           "phx-no-feedback:border-zinc-300 phx-no-feedback:focus:border-zinc-400",
           @errors == [] && "border-zinc-300 focus:border-zinc-400",
-          @errors != [] && "border-rose-400 focus:border-rose-400"
+          @errors != [] && "border-rose-400 focus:border-rose-400",
+          @label && "mt-2",
         ]}
         {@rest}
       />
@@ -458,7 +466,7 @@ defmodule BytebinderWeb.CoreComponents do
           <%= render_slot(@subtitle) %>
         </p>
       </div>
-      <div class="flex-none"><%= render_slot(@actions) %></div>
+      <div class="flex flex-row gap-2 items-center"><%= render_slot(@actions) %></div>
     </header>
     """
   end
