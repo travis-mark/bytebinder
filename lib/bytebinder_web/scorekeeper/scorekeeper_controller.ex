10a994da40c949a45bf9a6e0067678fe9877f90f
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2023-09-09 21:15:49 -0400

    Query games and scores

--- lib/bytebinder_web/scorekeeper/scorekeeper_controller.ex
+++ lib/bytebinder_web/scorekeeper/scorekeeper_controller.ex
@@ -1,7 +1,33 @@
+alias Bytebinder.{Repo,Score}
+
 defmodule BytebinderWeb.ScorekeeperController do
   use BytebinderWeb, :controller
+  import Ecto.Query
 
-  def index(conn, _params) do
-    render(conn, :index)
+  def index(conn, params) do
+    BytebinderWeb.UserAuth.fetch_current_user(conn, {})
+    if conn.assigns.current_user do
+      my_games = from(s in Score,
+        where: s.user_id == ^conn.assigns.current_user.id,
+        distinct: s.game,
+        select: s.game) |> Repo.all()
+      friends = []
+      other_games = []
+      scores_query = from(s in Score,
+        where: s.user_id == ^conn.assigns.current_user.id,
+        order_by: [desc: s.game_no])
+      scores_query = case Map.get(params, "game") do
+        nil -> scores_query
+        game -> scores_query |> where([s], s.game == ^game)
+      end
+      scores = Repo.all(scores_query) |> IO.inspect()
+      render(conn, :index,
+        my_games: my_games,
+        friends: friends,
+        other_games: other_games,
+        scores: scores)
+    else
+      redirect(conn, to: ~p"/users/log_in")
+    end
   end
 end
