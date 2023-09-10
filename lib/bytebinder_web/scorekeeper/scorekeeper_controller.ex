df15a6cec636584c3202d2907f1467459b54344a
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2023-09-09 22:00:06 -0400

    Copy CRUD from v1 and adjust

--- lib/bytebinder_web/scorekeeper/scorekeeper_controller.ex
+++ lib/bytebinder_web/scorekeeper/scorekeeper_controller.ex
@@ -14,13 +14,15 @@ defmodule BytebinderWeb.ScorekeeperController do
       friends = []
       other_games = []
       scores_query = from(s in Score,
-        where: s.user_id == ^conn.assigns.current_user.id,
-        order_by: [desc: s.game_no])
+        where: s.user_id == ^conn.assigns.current_user.id)
       scores_query = case Map.get(params, "game") do
         nil -> scores_query
-        game -> scores_query |> where([s], s.game == ^game)
+          |> order_by([s], desc: s.inserted_at)
+        game -> scores_query
+          |> where([s], s.game == ^game)
+          |> order_by([s], desc: s.game_no)
       end
-      scores = Repo.all(scores_query) |> IO.inspect()
+      scores = Repo.all(scores_query)
       render(conn, :index,
         my_games: my_games,
         friends: friends,
@@ -28,6 +30,65 @@ defmodule BytebinderWeb.ScorekeeperController do
         scores: scores)
     else
       redirect(conn, to: ~p"/users/log_in")
+    end
+  end
+
+    def new(conn, _params) do
+    BytebinderWeb.UserAuth.fetch_current_user(conn, {})
+    if conn.assigns.current_user do
+      changeset = Score.changeset(%Score{}, %{})
+      render(conn, :new, changeset: changeset)
+    else
+      redirect(conn, to: ~p"/users/log_in")
+    end
+  end
+
+  def create(conn, %{"score" => score_params}) do
+    BytebinderWeb.UserAuth.fetch_current_user(conn, {})
+    data = score_params
+    |> Map.merge(Score.classify(score_params["input"]) || %{})
+    |> Map.merge(%{"user_id" => conn.assigns.current_user.id})
+    changeset = Score.changeset(%Score{},  data)
+    case Repo.insert(changeset) do
+      {:ok, score} ->
+        if data["score"] do
+          conn
+          |> put_flash(:info, "#{Score.format(score)} added.")
+          |> redirect(to: ~p"/scorekeeper/")
+        else
+          conn
+          |> put_flash(:info, "Score started, please add additional details.")
+          |> redirect(to: ~p"/scorekeeper/#{score.id}/edit")
+        end
+
+      {:error, changeset} ->
+        render(conn, :new, changeset: changeset)
+    end
+  end
+
+    def show(conn, %{"id" => id}) do
+    score = Repo.get(Score, id)
+    changeset = Score.changeset(score, %{})
+    render(conn, :show, changeset: changeset)
+  end
+
+  def edit(conn, %{"id" => id}) do
+    score = Repo.get(Score, id)
+    changeset = Score.changeset(score, %{})
+    render(conn, :edit, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => id, "score" => score_params}) do
+    score = Repo.get(Score, id)
+    changeset = Score.changeset(score, score_params)
+    case Repo.update(changeset) do
+      {:ok, score} ->
+        conn
+        |> put_flash(:info, "#{Score.format(score)} updated.")
+        |> redirect(to: ~p"/scorekeeper/")
+
+      {:error, changeset} ->
+        render(conn, :edit, changeset: changeset)
     end
   end
 end
